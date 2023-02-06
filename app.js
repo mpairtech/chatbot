69db4fe15ec25199d6ed1acbf61ba79035bdeb23
mPair Tech <mpair.tach@gmail.com>
2023-02-06 17:10:29 +0600

    new update

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.post('/postdis', (req, res) => {
 app.post('/fdlist', (req, res) => {
   const { dname } = req.body;
 
-  db.query('SELECT *FROM disease WHERE output = ?', dname, (err, result) => {
+  db.query('SELECT *FROM disease WHERE output = ? LIMIT 10', dname, (err, result) => {
     res.json({ 'message': result });
     console.log(err)
   });
